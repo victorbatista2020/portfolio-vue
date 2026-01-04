9030455f7f7ebdeb1b037962b9b39544db22c695
victorbatista2020 <victorbatista2018@hotmail.com>
2026-01-04 13:41:52 -0300

    Update test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home é aqui porra</router-link> |
+    <router-link to="/about">About é o caralho</router-link>
   </nav>
   <router-view/>
 </template>
